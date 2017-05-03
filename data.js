fad91fcd6f6387c7839cfe19e1846db8a9a24a72
KrishnaXavier <tsixav@gmail.com>
2017-05-02 21:26:57 -0300

    primeiro commit

---
+++ data.js
@@ -0,0 +1,288 @@
+data = [
+{
+	"contato": "rh@gestorsa.net",
+	"nome": "Gestor",	
+	"endereco": "R. Félix da Cunha, 811",
+	"complemento": "",
+	"area": "",
+},
+
+{
+	"contato": "cristiane.oliveira@ag2nurun.com.br",
+	"nome": "Ag2 Nurun",		
+	"endereco": "Av. Dr. Figueiredo Mascarenhas",
+	"complemento": "",
+	"area": "",
+},
+
+{
+	"contato": "vagas@mariachi.com.br",
+	"nome": "Mariachi",		
+	"endereco": "R. Santos Dumont, 152C",
+	"complemento": "",
+	"area": "",
+},
+
+{
+	"contato": "contato@projetodesignio.com.br",
+	"nome": "Desígnio",
+	"endereco": "General Telles, 509",
+	"complemento": "",
+	"area": "",
+},
+
+{
+	"contato": "marinamimbarcas@hotmail.com",
+	"nome": "Marina Live Marketing",		
+	"endereco": "Miguel Barcelos, 579",
+	"complemento": "",
+	"area": "Design",
+},
+
+{
+	"contato": "contato@agenciarubra.com",
+	"nome": "Rubra",		
+	"endereco": "Rua XV de Novembro, 563/206",
+	"complemento": "",
+	"area": "Design",
+},
+
+{
+	"contato": "luisotavio.rs@webzero.com.br",
+	"nome": "WebZero",		
+	"endereco": "Rua Leonel de Moura Brizola, 650, casa j34 - Lagos de São Gonçalo",
+	"complemento": "",
+	"area": "",
+},
+
+{
+	"contato": "juntosna@garupa.com.vc",
+	"nome": "Garupa",		
+	"endereco": "R. Dr. Urbano Garcia, 51",
+	"complemento": "",
+	"area": "Design",
+},
+
+{
+	"contato": "contato@bravoagencia.com.br",
+	"nome": "Bravo",		
+	"endereco": "Rua Sete de Setembro, 274",
+	"complemento": "sala 304",
+	"area": "",
+},
+
+{
+	"contato": "atendimento@agenciapxp.com",
+	"nome": "Agência Pxp",		
+	"endereco": "Rua XV de Novembro, 617",
+	"complemento": "Sala 02",
+	"area": "",
+},
+
+{
+	"contato": "contato@lab507.com.br",
+	"nome": "LAB507",		
+	"endereco": "Rua XV de Novembro, 155",
+	"complemento": "",
+	"area": "",
+},
+
+{
+	"contato": "contact@nodo.cc",
+	"nome": "Nodo",		
+	"endereco": "Rua General Argolo, 872",
+	"complemento": "",
+	"area": "",
+},
+
+{
+	"contato": "contato@outdabox.in",
+	"nome": "Outabox",		
+	"endereco": "R. Alexandre Gastaud, 200",
+	"complemento": "",
+	"area": "",
+},
+{
+	"contato": "yes@yespropaganda.com",
+	"nome": "Yes Propaganda",		
+	"endereco": "Praça 7 de Julho, 36",
+	"complemento": "Sala 01",
+	"area": "Design",
+},
+
+{
+	"contato": "contato@jlcomunicacao.com",
+	"nome": "JL Comunicação e Desenvolvimento",		
+	"endereco": "Santos Dumont, 154",
+	"complemento": "sala 301",
+	"area": "",
+},
+
+{
+	"contato": "contato@quadroweb.com.br",
+	"nome": "Quadro Solução Web",		
+	"endereco": "Rua Padre Felício, 624A",
+	"complemento": "",
+	"area": "",
+},
+
+{
+	"contato": "contato@w8s.com.br",
+	"nome": "w8s",		
+	"endereco": "Rua Senador Mendonça Nº 301",
+	"complemento": "Sala 501",
+	"area": "",
+},
+
+{
+	"contato": "contato@checkplant.com.br",
+	"nome": "Checkplant",		
+	"endereco": "Rua XV de Novembro, 1539",
+	"complemento": "",
+	"area": "Design",
+},
+
+{
+	"contato": "sitionet@sitionet.com.br",
+	"nome": "Sitio Net",		
+	"endereco": "R. Princesa Isabel, 280",
+	"complemento": "Sala 701",
+	"area": "",
+},
+
+{
+	"contato": "atendimento@nativudesign.com.br",
+	"nome": "Nativu Design",		
+	"endereco": "R. Benjamin Constant, 1692",
+	"complemento": "",
+	"area": "Design",
+},
+
+{
+	"contato": "lsacramento@eckert-caine.com",
+	"nome": "Eckert Caine",		
+	"endereco": "Rua XV de Novembro, 667",
+	"complemento": "sala 502",
+	"area": "",
+},
+
+{
+	"contato": "PEL.careers@possible.com",
+	"nome": "Possible",		
+	"endereco": "Rua General Neto, 1035",
+	"complemento": "201",
+	"area": "",
+},
+
+{
+	"contato": "satolep@brave.ag",
+	"nome": "Brave",		
+	"endereco": "R. Santa Cruz, 2425",
+	"complemento": "",
+	"area": "",
+},
+
+{
+	"contato": "bstech@bstech.com.br",
+	"nome": "Bs Tech",		
+	"endereco": "R. Gomes Carneiro, 1209",
+	"complemento": "",
+	"area": "",
+},
+
+{
+	"contato": "work@izyplay.com.br",
+	"nome": "Izy Play Studio",		
+	"endereco": "Rua General Argolo, 467",
+	"complemento": "",
+	"area": "",
+},
+
+{
+	"contato": "contato@webhnet.com.br",
+	"nome": "WebHNet",		
+	"endereco": "R. Baldomero Trapaga, 90",
+	"complemento": "",
+	"area": "",
+},
+
+{
+	"contato": "contato@BrahmSoftware.com",
+	"nome": "BrahmSoftware",		
+	"endereco": "Rua Barão de Santa Tecla 1151 / 502",
+	"complemento": "",
+	"area": "",
+},
+
+{
+	"contato": "contato@i9naweb.com.br",
+	"nome": "i9 Web",		
+	"endereco": "R. Barão de Santa Tecla, 515",
+	"complemento": "",
+	"area": "",
+},
+
+{
+	"contato": "vagas@hostche.com.br",
+	"nome": "Hostche",		
+	"endereco": "Rua General Neto, 915",
+	"complemento": "Sala 303",
+	"area": "",
+},
+
+{
+	"contato": "contato@k7web.com.br",
+	"nome": "K7 Web",		
+	"endereco": "Princesa Isabel, 85",
+	"complemento": "",
+	"area": "",
+},
+
+{
+	"contato": "http://www.comparte.com.br/site/#contact",
+	"nome": "Comparte",		
+	"endereco": "R. Marechal Deodoro, 702 A",
+	"complemento": "",
+	"area": "",
+},
+
+{
+	"contato": "http://www.create.art.br/teaser/",
+	"nome": "Create Pensamentos Online",		
+	"endereco": "Rua Santa Tecla, 583",
+	"complemento": "Cobertura",
+	"area": "",
+},
+
+{
+	"contato": "https://www.plugzone.net",
+	"nome": "PlugZone",		
+	"endereco": "Rua Conselheiro Silveira Martins nº 547A",
+	"complemento": "",
+	"area": "",
+},
+
+{
+	"contato": "contato@agenciadesenvolve.com.br",
+	"nome": "Agência Desenvolve",		
+	"endereco": " Av. General Abreu Lima, 39",
+	"complemento": "",
+	"area": "",
+},
+
+{
+	"contato": "rh@voiza.com.br",
+	"nome": "Vooiza",		
+	"endereco": "Rua Três de maio, 1005",
+	"complemento": "",
+	"area": "",
+},
+
+{
+	"contato": "pelotas@incomum.in",
+	"nome": "Incomum",		
+	"endereco": "Rua Gonçalves Chaves, 3542",
+	"complemento": "",
+	"area": "",
+}
+]
